990d553c7efd91f0dd034b2d44668803cdd94bdc
Aviad Haham <ahaham@nexxen.com>
2023-12-16 23:16:09 +0200

    add results count and table row numbers for each result

--- static/index.js
+++ static/index.js
@@ -4,6 +4,7 @@ const resourceNameInput = document.getElementById('resource-name');
 const resourceTypeSelect = document.getElementById('resource-type');
 const resultsDiv = document.getElementById('search-results');
 const searchSpinner = document.getElementById('search-spinner');
+const resultsCount = document.getElementById('results-count');
 
 // Form submit handler
 form.addEventListener('submit', (e) => {
@@ -14,40 +15,62 @@ form.addEventListener('submit', (e) => {
   const resourceName = resourceNameInput.value;
   const resourceType = resourceTypeSelect.value;
 
-    // Make POST request to API
-    const url = `search/?resource_name=${resourceName}&resource_type=${resourceType}`;
-    searchSpinner.style.display = 'block';
-    resultsDiv.innerHTML = '';
-    fetch(url)
+  if (resourceName === '') {
+    console.log('Resource name is empty.');
+    return; // Do not trigger the event if resourceName is empty
+  }
+
+  // Make POST request to API
+  const url = `search/?resource_name=${resourceName}&resource_type=${resourceType}`;
+  searchSpinner.style.display = 'block';
+  resultsCount.style.display = 'block';
+  resultsCount.innerHTML = '';
+  resultsDiv.innerHTML = '';
+  fetch(url)
     .then(response => response.json())
     .then(data => {
-        // Build table dynamically
-        let table = resultsDiv.appendChild(document.createElement('table'));
-        table.style.border = '1px solid black';
-        table.style.borderCollapse = 'collapse';
+      // Build table dynamically
+      let table = resultsDiv.appendChild(document.createElement('table'));
+      table.style.border = '1px solid black';
+      table.style.borderCollapse = 'collapse';
 
+      let tr = table.appendChild(document.createElement('tr'));
+      const thRowNumber = document.createElement('th');
+      thRowNumber.innerText = '#';
+      thRowNumber.style.border = '1px solid black';
+      thRowNumber.style.padding = '5px';
+      tr.appendChild(thRowNumber);
+
+      Object.keys(data.results[0]).forEach((column) => {
+        const th = document.createElement('th');
+        th.innerText = column;
+        th.style.border = '1px solid black';
+        th.style.padding = '5px';
+        tr.appendChild(th);
+      });
+
+      data.results.forEach((result, index) => {
         let tr = table.appendChild(document.createElement('tr'));
-        Object.keys(data.results[0]).forEach((column) => {
-            const th = document.createElement('th');
-            th.innerText = column;
-            th.style.border = '1px solid black';
-            th.style.padding = '5px';
-            tr.appendChild(th);
-        });
+        const tdRowNumber = document.createElement('td');
+        tdRowNumber.innerText = index + 1;
+        tdRowNumber.style.border = '1px solid black';
+        tdRowNumber.style.padding = '5px';
+        tr.appendChild(tdRowNumber);
 
-        data.results.forEach((result) => {
-            let tr = table.appendChild(document.createElement('tr'));
-            Object.values(result).forEach((value) => {
-                const td = document.createElement('td');
-                td.innerText = value;
-                td.style.border = '1px solid black';
-                td.style.padding = '5px';
-                tr.appendChild(td);
-            });
+        Object.values(result).forEach((value) => {
+          const td = document.createElement('td');
+          td.innerText = value;
+          td.style.border = '1px solid black';
+          td.style.padding = '5px';
+          tr.appendChild(td);
         });
+      });
+
+      resultsCount.innerText = `Results: ${data.results.length}`;
     })
     .catch((error) => {
       console.log(error);
+      resultsCount.innerText = 'Error occurred while fetching results.';
     })
     .finally(() => {
       searchSpinner.style.display = 'none';
