83a0737a3a6e61c5621d89ff4cbb4230b66cc0f4
Aviad Haham <aviadhaham@outlook.com>
2024-06-14 18:56:42 +0300

    fix: rename repo to cloudcate

--- internal/web/router.go
+++ internal/web/router.go
@@ -4,8 +4,8 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/aviadhaham/cloudcate/internal/aws_search/config"
-	"github.com/aviadhaham/cloudcate/internal/aws_search/search"
+	config "github.com/aviadhaham/cloudcate/internal/aws_search/config"
+	search "github.com/aviadhaham/cloudcate/internal/aws_search/search"
 
 	"github.com/gin-gonic/contrib/static"
 	"github.com/gin-gonic/gin"
